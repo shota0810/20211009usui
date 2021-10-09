421aa36602d7aad2f883f205acf80046d0213f37
shota <syota.u1104@gmail.com>
2021-10-09 23:05:09 +0900

    second commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<table border='1' style="border-collapse: collapse">
+        <?php for($i=1;$i<=9;$i++){
+            echo '<tr>';
+            for($j=1;$j<=9;$j++){
+                echo '<td>'.$i*$j.'</td>';
+            }
+            echo '</tr>';
+        } ?>
+</table>
